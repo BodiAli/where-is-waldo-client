39a83a21f18d0ba3c3d99dad8bfaf276a4301261
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-22 18:51:44 +0300

    Feat: Allow compiler to include css modules and images

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,1 +1,13 @@
+declare module "*.module.css" {
+  const classes: Record<string, string>;
+  export default classes;
+}
 declare module "*.css";
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
