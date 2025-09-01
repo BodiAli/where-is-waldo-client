63ffd7ce0d1e9766bcd21ef5c2d11560f50feefa
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-02 00:22:45 +0300

    Feat: Create type for vite env object

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -11,3 +11,10 @@ declare module "*.jpeg" {
   const content: string;
   export default content;
 }
+interface ImportMetaEnv {
+  readonly VITE_SERVER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
