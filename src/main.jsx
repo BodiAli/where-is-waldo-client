e48e430fab0d4a84c4852c958edc166644629fa0
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-01 07:16:14 +0300

    Feat: Install roboto font and use it in app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./reset.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes/routes";
+import "./globals/reset.css";
+import "./globals/global.css";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
