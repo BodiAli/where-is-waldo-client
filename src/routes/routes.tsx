8f6f3736ac62d679e0cb5a6f472630e8c00e0af2
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-02 21:54:20 +0300

    Feat: Add ErrorBoundary for not found rotues and unexpected errors

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,18 +2,27 @@ import type { RouteObject } from "react-router";
 import AppLayout from "../components/AppLayout/AppLayout";
 import IllustrationPage from "../pages/Illustration/IllustrationPage";
 import IllustrationsPage from "../pages/Illustrations/IllustrationsPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import ErrorHandler from "../components/ErrorHandler/ErrorHandler";
 
 const routes: RouteObject[] = [
   {
+    ErrorBoundary: NotFoundPage,
     Component: AppLayout,
+
     children: [
       {
-        index: true,
-        Component: IllustrationsPage,
-      },
-      {
-        path: "illustrations/:illustrationId",
-        Component: IllustrationPage,
+        ErrorBoundary: ErrorHandler,
+        children: [
+          {
+            index: true,
+            Component: IllustrationsPage,
+          },
+          {
+            path: "illustrations/:illustrationId",
+            Component: IllustrationPage,
+          },
+        ],
       },
     ],
   },
