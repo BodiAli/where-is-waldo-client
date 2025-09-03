fac77eb2f2d08be31f70582148486af28ed06a8d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-04 00:40:03 +0300

    Feat: Create an IllustrationCard component to render Illustration details

---
+++ src/components/IllustrationCard/IllustrationCard.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router";
+import type { IllustrationType } from "../../types/types";
+import styles from "./IllustrationCard.module.css";
+
+export default function IllustrationCard({ id, imageId, difficulty }: Omit<IllustrationType, "Characters">) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.card}>
+      <h2>{difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}</h2>
+      <img src={imageId} alt={`${difficulty} illustration`} />
+      <button
+        type="button"
+        onClick={() => {
+          void navigate(`/illustrations/${id}`);
+        }}
+      >
+        Start
+      </button>
+    </div>
+  );
+}
