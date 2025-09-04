4f2d6cc6725a819958ee9fb66eba8327bac7e31e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-04 07:38:52 +0300

    Feat: Create tests for IllustrationComponent

---
+++ src/tests/unit/IllustrationComponent.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Illustration from "../../components/Illustration/Illustration";
+import type { IllustrationType } from "../../types/types";
+
+describe("Illustration component", () => {
+  it("should render illustration", () => {
+    const mockIllustration: IllustrationType = {
+      difficulty: "medium",
+      imageId: "imageId",
+      id: "illustrationId",
+      Characters: [
+        {
+          id: "waldoId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Waldo",
+          imageSrc: "waldoImg",
+        },
+        {
+          id: "wendaId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Wenda",
+          imageSrc: "wendaImg",
+        },
+        {
+          id: "wizardId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Wizard",
+          imageSrc: "wizrdImg",
+        },
+      ],
+    };
+
+    const mockHandleClick = vi.fn();
+
+    render(<Illustration illustration={mockIllustration} onClick={mockHandleClick} message={null} />);
+
+    expect(screen.getByRole("img", { name: "medium illustration" })).toBeInTheDocument();
+  });
+
+  it("should call onClick prop when image is clicked", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: IllustrationType = {
+      difficulty: "medium",
+      imageId: "imageId",
+      id: "illustrationId",
+      Characters: [
+        {
+          id: "waldoId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Waldo",
+          imageSrc: "waldoImg",
+        },
+        {
+          id: "wendaId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Wenda",
+          imageSrc: "wendaImg",
+        },
+        {
+          id: "wizardId",
+          illustrationId: "illustrationId",
+          isFound: false,
+          name: "Wizard",
+          imageSrc: "wizrdImg",
+        },
+      ],
+    };
+
+    const mockHandleClick = vi.fn();
+
+    render(<Illustration illustration={mockIllustration} onClick={mockHandleClick} message={null} />);
+
+    const illustration = screen.getByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    expect(mockHandleClick).toHaveBeenCalled();
+  });
+
+  it.skip("should render a flash message when a message is passed", () => {});
+});
