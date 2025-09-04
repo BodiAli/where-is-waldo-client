20c3f70ef04c27f61dd3a4bbfe1d8bcecf45f419
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-04 04:21:48 +0300

    Feat: Handle edge cases and errors

--- src/pages/Illustration/IllustrationPage.tsx
+++ src/pages/Illustration/IllustrationPage.tsx
@@ -1,3 +1,22 @@
+import { useParams } from "react-router";
+import useFetchSingleIllustration from "../../hooks/useFetchSingleIllustration";
+import Loader from "../../components/Loader/Loader";
+import styles from "./IllustrationPage.module.css";
+
 export default function IllustrationPage() {
-  return <main>Illustration page</main>;
+  const { illustrationId } = useParams();
+
+  if (!illustrationId) {
+    throw new Error("Illustration not found");
+  }
+
+  const { illustration, error, loading } = useFetchSingleIllustration(`/illustrations/${illustrationId}`);
+
+  if (loading || !illustration) return <Loader />;
+
+  if (error) throw new Error(error.message);
+
+  console.log(illustration);
+
+  return <main className={styles.main}></main>;
 }
