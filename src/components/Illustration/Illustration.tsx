2405e1ef3dbce10003dc02c78152460d6e2fe168
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-05 12:04:53 +0300

    Feat: Pass necessary drops to complete logic of showing rendering a characters dropdown

--- src/components/Illustration/Illustration.tsx
+++ src/components/Illustration/Illustration.tsx
@@ -1,19 +1,45 @@
-import type { MouseEvent } from "react";
+import type { MouseEvent, Ref } from "react";
 import type { IllustrationType } from "../../types/types";
 import styles from "./Illustration.module.css";
 
 export default function Illustration({
   message,
   illustration,
-  onClick,
+  onShowDropdown,
+  onClickDropdown,
+  dropdownPosition,
+  dropdownRef,
 }: {
   illustration: IllustrationType;
   message: string | null;
-  onClick: (e: MouseEvent<HTMLImageElement>) => void;
+  onShowDropdown: (e: MouseEvent<HTMLImageElement>) => void;
+  onClickDropdown: (e: MouseEvent<HTMLDivElement>) => void;
+  dropdownPosition: { top: number; left: number };
+  dropdownRef: Ref<HTMLDivElement>;
 }) {
   return (
     <section className={styles.section}>
-      <img src={illustration.imageId} alt={`${illustration.difficulty} illustration`} onClick={onClick} />
+      <div
+        ref={dropdownRef}
+        className={styles.dropdown}
+        style={{ left: dropdownPosition.left, top: dropdownPosition.top }}
+      >
+        <ul>
+          {illustration.Characters.map((character) => {
+            return (
+              <div key={character.id} className={styles.characterCard} onClick={onClickDropdown}>
+                <img src={character.imageSrc} alt={`${character.name} character`} />
+                <p>{character.name}</p>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <img
+        src={illustration.imageId}
+        alt={`${illustration.difficulty} illustration`}
+        onClick={onShowDropdown}
+      />
     </section>
   );
 }
