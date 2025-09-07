668f7e8c0b3d227cb1506eeae5c4faf953456153
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-08 02:20:53 +0300

    Fix: Update field name to sync with server field name

--- src/components/IllustrationCard/IllustrationCard.tsx
+++ src/components/IllustrationCard/IllustrationCard.tsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router";
 import type { IllustrationType } from "../../types/types";
 import styles from "./IllustrationCard.module.css";
 
-export default function IllustrationCard({ id, imageId, difficulty }: Omit<IllustrationType, "Characters">) {
+export default function IllustrationCard({ id, imageSrc, difficulty }: Omit<IllustrationType, "Characters">) {
   const navigate = useNavigate();
 
   return (
     <div className={styles.card}>
       <h2>{difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}</h2>
-      <img src={imageId} alt={`${difficulty} illustration`} />
+      <img src={imageSrc} alt={`${difficulty} illustration`} />
       <button
         type="button"
         onClick={() => {
