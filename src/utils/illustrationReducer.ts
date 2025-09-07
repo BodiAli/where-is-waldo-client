8ba03db5cc73b6baa5e11fa1e255852c644c20a9
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-08 02:24:43 +0300

    Feat: Implement logic to update character isFound

--- src/utils/illustrationReducer.ts
+++ src/utils/illustrationReducer.ts
@@ -2,9 +2,21 @@ import type { ACTION_TYPE, IllustrationType } from "../types/types";
 
 export default function illustrationReducer(state: IllustrationType | null, action: ACTION_TYPE) {
   switch (action.type) {
-    case "fetch-illustration":
+    case "fetch-illustration": {
       return action.payload;
-    case "update-character":
-      return state;
+    }
+    case "update-character": {
+      if (!state) {
+        throw new Error("Illustration not found");
+      }
+      const updatedCharacters = state.Characters.map((character) => {
+        if (character.id === action.payload.id) {
+          return action.payload;
+        }
+        return character;
+      });
+
+      return { ...state, Characters: updatedCharacters };
+    }
   }
 }
