f43b80c6b9783d29176a64faea798b137e40c648
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-08 02:23:49 +0300

    Feat: Implement validating character position

--- src/pages/Illustration/IllustrationPage.tsx
+++ src/pages/Illustration/IllustrationPage.tsx
@@ -6,10 +6,14 @@ import Loader from "../../components/Loader/Loader";
 import Characters from "../../components/Characters/Characters";
 import Illustration from "../../components/Illustration/Illustration";
 import styles from "./IllustrationPage.module.css";
+import { SERVER_URL, type CharacterType } from "../../types/types";
+import getImageCoordinates from "../../utils/getImageCoordinates";
 
 export default function IllustrationPage() {
   const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
   const [isDropdownShown, setIsDropdownShown] = useState(false);
+  const [imgCoordinates, setImgCoordinates] = useState<{ xPosition: number; yPosition: number } | null>(null);
+  const [message, setMessage] = useState("");
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const { illustrationId } = useParams();
@@ -18,7 +22,9 @@ export default function IllustrationPage() {
     throw new Error("Illustration not found");
   }
 
-  const { illustration, error, loading } = useFetchSingleIllustration(`/illustrations/${illustrationId}`);
+  const { illustration, error, loading, dispatch } = useFetchSingleIllustration(
+    `/illustrations/${illustrationId}`
+  );
 
   if (loading || !illustration) return <Loader />;
 
@@ -34,6 +40,10 @@ export default function IllustrationPage() {
       // Return early because it's unnecessary for the rest of the function to run if dropdown is hidden
       return;
     }
+
+    const { imgX, imgY } = getImageCoordinates(e);
+
+    setImgCoordinates({ xPosition: imgX, yPosition: imgY });
     setIsDropdownShown(true);
 
     const imgRect = e.currentTarget.getBoundingClientRect();
@@ -85,7 +95,47 @@ export default function IllustrationPage() {
     }
   }
 
-  function handleValidateCharacter(e: MouseEvent<HTMLDivElement>) {}
+  function handleValidateCharacter(e: MouseEvent<HTMLDivElement>) {
+    const { characterid: characterId } = e.currentTarget.dataset;
+
+    if (!illustration) {
+      throw new Error("Illustration not found");
+    }
+    if (!characterId) {
+      throw new Error("Character not found");
+    }
+
+    const handler = async () => {
+      try {
+        const res = await fetch(`${SERVER_URL}/illustrations/${illustration.id}/${characterId}`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(imgCoordinates),
+        });
+
+        if (!res.ok) {
+          const { error } = (await res.json()) as { error: string };
+
+          setMessage(error);
+          return;
+        }
+
+        const { character, msg } = (await res.json()) as {
+          character: CharacterType;
+          msg: string;
+        };
+
+        dispatch({ type: "update-character", payload: character });
+        setMessage(msg);
+      } catch (error) {
+        throw new Error((error as Error).message);
+      }
+    };
+
+    void handler();
+  }
 
   return (
     <main className={styles.main}>
@@ -98,7 +148,7 @@ export default function IllustrationPage() {
         dropdownPosition={dropdownPosition}
         dropdownRef={dropdownRef}
         isDropdownShown={isDropdownShown}
-        message={null}
+        message={message}
       />
     </main>
   );
