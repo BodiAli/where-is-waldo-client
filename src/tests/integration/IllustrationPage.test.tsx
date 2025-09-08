dce11628dda213f855f35d9b50bbd6dcf80a2a9c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-08 09:45:44 +0300

    Feat: Create test for handling error when illustration is not found

--- src/tests/integration/IllustrationPage.test.tsx
+++ src/tests/integration/IllustrationPage.test.tsx
@@ -3,13 +3,14 @@ import type { Mock } from "vitest";
 import { createMemoryRouter, RouterProvider } from "react-router";
 import { SERVER_URL, type IllustrationType } from "../../types/types";
 import IllustrationPage from "../../pages/Illustration/IllustrationPage";
+import ErrorHandler from "../../components/ErrorHandler/ErrorHandler";
 
 describe("Illustration Page component", () => {
   it("should fetch illustration when valid illustration param is present", async () => {
     const mockIllustration: { illustration: IllustrationType } = {
       illustration: {
         difficulty: "medium",
-        imageId: "imageId",
+        imageSrc: "imageSrc",
         id: "illustrationId",
         Characters: [
           {
@@ -69,5 +70,30 @@ describe("Illustration Page component", () => {
     expect(window.fetch).toBeCalledWith(`${SERVER_URL}/illustrations/illustrationId`);
   });
 
-  it.skip("should render ErrorBoundary with a not found message when illustration is not found", async () => {});
+  it("should render ErrorBoundary with a not found message when illustration is not found", async () => {
+    window.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: false,
+        status: 404,
+        json: vi.fn(() => Promise.resolve({ error: "Illustration not found" })),
+      })
+    ) as Mock;
+
+    const router = createMemoryRouter(
+      [
+        {
+          ErrorBoundary: ErrorHandler,
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: ["/illustrations/invalidId"] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const notFoundMessage = await screen.findByRole("paragraph");
+
+    expect(notFoundMessage).toHaveTextContent("Illustration not found");
+  });
 });
