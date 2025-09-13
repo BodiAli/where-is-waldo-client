926d905477fd060ec3c84d8b5dd4c159f0e9a714
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-13 10:08:23 +0300

    Feat: Add tests for game logic

--- src/tests/integration/IllustrationPage.test.tsx
+++ src/tests/integration/IllustrationPage.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import type { Mock } from "vitest";
 import { createMemoryRouter, RouterProvider } from "react-router";
-import { SERVER_URL, type IllustrationType } from "../../types/types";
+import { SERVER_URL, type CharacterType, type IllustrationType } from "../../types/types";
 import IllustrationPage from "../../pages/Illustration/IllustrationPage";
 import ErrorHandler from "../../components/ErrorHandler/ErrorHandler";
+
+vi.mock("../../utils/getImageCoordinates.ts", () => {
+  return {
+    default: () => ({ imgX: 100, imgY: 100 }),
+  };
+});
 
 describe("Illustration Page component", () => {
   it("should fetch illustration when valid illustration param is present", async () => {
@@ -95,5 +102,411 @@ describe("Illustration Page component", () => {
     const notFoundMessage = await screen.findByRole("paragraph");
 
     expect(notFoundMessage).toHaveTextContent("Illustration not found");
+  });
+
+  it("should show a characters dropdown when the illustration is clicked", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: { illustration: IllustrationType } = {
+      illustration: {
+        difficulty: "medium",
+        imageSrc: "imageSrc",
+        id: "illustrationId",
+        Characters: [
+          {
+            id: "waldoId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Waldo",
+            imageSrc: "waldoImg",
+          },
+          {
+            id: "wendaId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wenda",
+            imageSrc: "wendaImg",
+          },
+          {
+            id: "wizardId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wizard",
+            imageSrc: "wizrdImg",
+          },
+        ],
+      },
+    };
+
+    window.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: vi.fn(() => Promise.resolve(mockIllustration)),
+      })
+    ) as Mock;
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: [`/illustrations/${mockIllustration.illustration.id}`] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const charactersDropdown = await screen.findByTestId("characters-dropdown");
+
+    expect(charactersDropdown).not.toBeVisible();
+
+    const illustration = await screen.findByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    expect(charactersDropdown).toBeVisible();
+  });
+
+  it("should show a try again message when the user clicks an invalid character position", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: { illustration: IllustrationType } = {
+      illustration: {
+        difficulty: "medium",
+        imageSrc: "imageSrc",
+        id: "illustrationId",
+        Characters: [
+          {
+            id: "waldoId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Waldo",
+            imageSrc: "waldoImg",
+          },
+          {
+            id: "wendaId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wenda",
+            imageSrc: "wendaImg",
+          },
+          {
+            id: "wizardId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wizard",
+            imageSrc: "wizardImg",
+          },
+        ],
+      },
+    };
+
+    const mockClickResponse: { character: CharacterType; msg: string; success: boolean } = {
+      character: {
+        id: "waldoId",
+        illustrationId: "illustrationId",
+        isFound: false,
+        imageSrc: "waldoImage",
+        name: "Waldo",
+      },
+      msg: "Try again",
+      success: false,
+    };
+
+    window.fetch = vi
+      .fn()
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockIllustration)),
+        })
+      )
+      .mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockClickResponse)),
+        })
+      );
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: [`/illustrations/${mockIllustration.illustration.id}`] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const illustration = await screen.findByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    const characterCards = await screen.findAllByTestId("character-card");
+
+    await user.click(characterCards[0]);
+
+    expect(await screen.findByTestId("flash-message")).toHaveTextContent("Try again");
+  });
+
+  it("should show a character is already found message when the user clicks an already found character", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: { illustration: IllustrationType } = {
+      illustration: {
+        difficulty: "medium",
+        imageSrc: "imageSrc",
+        id: "illustrationId",
+        Characters: [
+          {
+            id: "waldoId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Waldo",
+            imageSrc: "waldoImg",
+          },
+          {
+            id: "wendaId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wenda",
+            imageSrc: "wendaImg",
+          },
+          {
+            id: "wizardId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wizard",
+            imageSrc: "wizardImg",
+          },
+        ],
+      },
+    };
+
+    const mockClickResponse: { character: CharacterType; msg: string; success: boolean } = {
+      character: {
+        id: "waldoId",
+        illustrationId: "illustrationId",
+        isFound: true,
+        imageSrc: "waldoImage",
+        name: "Waldo",
+      },
+      msg: "Waldo is already found",
+      success: false,
+    };
+
+    window.fetch = vi
+      .fn()
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockIllustration)),
+        })
+      )
+      .mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockClickResponse)),
+        })
+      );
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: [`/illustrations/${mockIllustration.illustration.id}`] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const illustration = await screen.findByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    const characterCards = await screen.findAllByTestId("character-card");
+
+    await user.click(characterCards[0]);
+
+    expect(await screen.findByTestId("flash-message")).toHaveTextContent("Waldo is already found");
+  });
+
+  it("should show a success message when the user clicks a valid character position", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: { illustration: IllustrationType } = {
+      illustration: {
+        difficulty: "medium",
+        imageSrc: "imageSrc",
+        id: "illustrationId",
+        Characters: [
+          {
+            id: "waldoId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Waldo",
+            imageSrc: "waldoImg",
+          },
+          {
+            id: "wendaId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wenda",
+            imageSrc: "wendaImg",
+          },
+          {
+            id: "wizardId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wizard",
+            imageSrc: "wizardImg",
+          },
+        ],
+      },
+    };
+
+    const mockClickResponse: { character: CharacterType; msg: string; success: boolean } = {
+      character: {
+        id: "waldoId",
+        illustrationId: "illustrationId",
+        isFound: true,
+        imageSrc: "waldoImage",
+        name: "Waldo",
+      },
+      msg: "You found Waldo",
+      success: true,
+    };
+
+    window.fetch = vi
+      .fn()
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockIllustration)),
+        })
+      )
+      .mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockClickResponse)),
+        })
+      );
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: [`/illustrations/${mockIllustration.illustration.id}`] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const illustration = await screen.findByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    const characterCards = await screen.findAllByTestId("character-card");
+
+    await user.click(characterCards[0]);
+
+    expect(await screen.findByTestId("flash-message")).toHaveTextContent("You found Waldo");
+  });
+
+  it("should add a 'found' class to character icon when the corresponding character is found", async () => {
+    const user = userEvent.setup();
+
+    const mockIllustration: { illustration: IllustrationType } = {
+      illustration: {
+        difficulty: "medium",
+        imageSrc: "imageSrc",
+        id: "illustrationId",
+        Characters: [
+          {
+            id: "waldoId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Waldo",
+            imageSrc: "waldoImg",
+          },
+          {
+            id: "wendaId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wenda",
+            imageSrc: "wendaImg",
+          },
+          {
+            id: "wizardId",
+            illustrationId: "illustrationId",
+            isFound: false,
+            name: "Wizard",
+            imageSrc: "wizardImg",
+          },
+        ],
+      },
+    };
+
+    const mockClickResponse: { character: CharacterType; msg: string; success: boolean } = {
+      character: {
+        id: "waldoId",
+        illustrationId: "illustrationId",
+        isFound: true,
+        imageSrc: "waldoImage",
+        name: "Waldo",
+      },
+      msg: "You found Waldo",
+      success: true,
+    };
+
+    window.fetch = vi
+      .fn()
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockIllustration)),
+        })
+      )
+      .mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: vi.fn(() => Promise.resolve(mockClickResponse)),
+        })
+      );
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/illustrations/:illustrationId",
+          Component: IllustrationPage,
+        },
+      ],
+      { initialEntries: [`/illustrations/${mockIllustration.illustration.id}`] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const illustration = await screen.findByRole("img", { name: "medium illustration" });
+
+    await user.click(illustration);
+
+    const characterCards = await screen.findAllByTestId("character-card");
+
+    await user.click(characterCards[0]);
+
+    const waldoIcon = await screen.findByRole("img", { name: "Waldo character" });
+
+    expect(waldoIcon).toHaveClass(/found/);
+  });
+
+  it("should display a modal to ask for username after all characters are found", async () => {
+    // IMPLEMENT
   });
 });
