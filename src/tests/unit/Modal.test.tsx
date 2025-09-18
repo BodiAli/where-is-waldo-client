6ff294815f4c7581ea2dd83a654ee7aa7653f758
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-18 17:24:57 +0300

    Feat: Complete tests for Modal component

--- src/tests/unit/Modal.test.tsx
+++ src/tests/unit/Modal.test.tsx
@@ -1,18 +1,41 @@
-import { createMemoryRouter, RouterProvider } from "react-router";
-import Modal from "../../components/Modal/Modal";
 import { render, screen } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router";
+import type { RefObject } from "react";
+import Modal from "../../components/Modal/Modal";
 
 describe("Modal component", () => {
   it("should render a dialog component", () => {
+    const dialogRef: RefObject<HTMLDialogElement> = {
+      current: document.createElement("dialog"),
+    };
+
     const router = createMemoryRouter([
       {
         path: "/",
-        Component: Modal,
+        element: <Modal dialogRef={dialogRef} duration={1000} />,
       },
     ]);
 
     render(<RouterProvider router={router} />);
 
-    expect(screen.getByRole("dialog")).toBeInTheDocument();
+    expect(screen.getByRole("dialog", { hidden: true })).toBeInTheDocument();
+  });
+
+  it("should render how much time it took the user to win the game", () => {
+    const dialogRef: RefObject<HTMLDialogElement> = {
+      current: document.createElement("dialog"),
+    };
+
+    const router = createMemoryRouter([
+      {
+        path: "/",
+        element: <Modal dialogRef={dialogRef} duration={1000} />,
+      },
+    ]);
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByRole("heading", { level: 2, name: "You won!", hidden: true })).toBeInTheDocument();
+    expect(screen.getByTestId("score")).toHaveTextContent("It took you 1.00 seconds to complete.");
   });
 });
