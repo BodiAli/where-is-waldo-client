7eb348145c234fa572de70413f6e7aac5a8a2495
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-20 14:11:34 +0300

    Feat: Add routes for leaderbords paths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,8 @@ import AppLayout from "../components/AppLayout/AppLayout";
 import IllustrationPage from "../pages/Illustration/IllustrationPage";
 import IllustrationsPage from "../pages/Illustrations/IllustrationsPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import Leaderboards from "../pages/Leaderboards/Leaderboards";
+import Leaderboard from "../pages/Leaderboard/Leaderboard";
 import ErrorHandler from "../components/ErrorHandler/ErrorHandler";
 
 const routes: RouteObject[] = [
@@ -21,6 +23,14 @@ const routes: RouteObject[] = [
           {
             path: "illustrations/:illustrationId",
             Component: IllustrationPage,
+          },
+          {
+            path: "leaderboards",
+            Component: Leaderboards,
+          },
+          {
+            path: "leaderboards/:leaderboardId",
+            Component: Leaderboard,
           },
         ],
       },
