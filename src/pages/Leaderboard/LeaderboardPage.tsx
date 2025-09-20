27b09e747ad8aab68968d42575ae756732ae2612
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-20 17:38:17 +0300

    Feat: Create logic to get leaderboard

--- src/pages/Leaderboard/LeaderboardPage.tsx
+++ src/pages/Leaderboard/LeaderboardPage.tsx
@@ -1,3 +1,26 @@
+import { useParams } from "react-router";
+import useFetchSingleLeaderboard from "../../hooks/useFetchSingleLeaderboard";
+import Loader from "../../components/Loader/Loader";
+import type { LeaderboardType } from "../../types/types";
+
 export default function LeaderboardPage() {
-  return <p>ZOZO</p>;
+  const { leaderboardId } = useParams();
+
+  if (!leaderboardId) {
+    throw new Error("Leaderboard not found");
+  }
+
+  const { data, loading, error } = useFetchSingleLeaderboard(leaderboardId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  const { leaderboard } = data as { leaderboard: LeaderboardType };
+
+  return <main></main>;
 }
