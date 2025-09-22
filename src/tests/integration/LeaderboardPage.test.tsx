125a54e927a039f6303786196128a48f4a370289
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-09-22 13:44:59 +0300

    Feat: Create tests for rendering users && fix Users order because the server sends users ordered by their score

--- src/tests/integration/LeaderboardPage.test.tsx
+++ src/tests/integration/LeaderboardPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router";
 import LeaderboardPage from "../../pages/Leaderboard/LeaderboardPage";
 import ErrorHandler from "../../components/ErrorHandler/ErrorHandler";
@@ -44,16 +44,16 @@ describe("LeaderboardPage component", () => {
         },
         Users: [
           {
-            id: "joeId",
-            leaderboardId: "leaderboardId",
-            name: "Joe",
-            score: 5000,
-          },
-          {
             id: "bodiId",
             leaderboardId: "leaderboardId",
             name: "Bodi",
             score: 4000,
+          },
+          {
+            id: "joeId",
+            leaderboardId: "leaderboardId",
+            name: "Joe",
+            score: 5000,
           },
           {
             id: "janeId",
@@ -91,5 +91,141 @@ describe("LeaderboardPage component", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  it("should render users ordered by their scores", () => {});
+  it("should render users table with expected column headers", async () => {
+    const mockLeaderboard: { leaderboard: LeaderboardType } = {
+      leaderboard: {
+        id: "leaderboardId",
+        illustrationId: "illustrationId",
+        Illustration: {
+          difficulty: "medium",
+          id: "illustrationId",
+          imageSrc: "imageSrc",
+        },
+        Users: [
+          {
+            id: "bodiId",
+            leaderboardId: "leaderboardId",
+            name: "Bodi",
+            score: 4000,
+          },
+          {
+            id: "joeId",
+            leaderboardId: "leaderboardId",
+            name: "Joe",
+            score: 5000,
+          },
+          {
+            id: "janeId",
+            leaderboardId: "leaderboardId",
+            name: "Jane",
+            score: 6000,
+          },
+        ],
+      },
+    };
+
+    window.fetch = vi.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: vi.fn(() => Promise.resolve(mockLeaderboard)),
+      })
+    );
+
+    const router = createMemoryRouter(
+      [
+        {
+          Component: LeaderboardPage,
+          path: "leaderboards/:leaderboardId",
+        },
+      ],
+      {
+        initialEntries: ["/leaderboards/leaderboardId"],
+      }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const table = await screen.findByRole("table");
+
+    const columnHeaders = within(table).getAllByRole("columnheader");
+
+    expect(columnHeaders.length).toBe(3);
+    expect(columnHeaders[0]).toHaveTextContent("Rank");
+    expect(columnHeaders[1]).toHaveTextContent("Name");
+    expect(columnHeaders[2]).toHaveTextContent("Score");
+  });
+
+  it("should render users sent by the server with their rank and score converted to seconds", async () => {
+    const mockLeaderboard: { leaderboard: LeaderboardType } = {
+      leaderboard: {
+        id: "leaderboardId",
+        illustrationId: "illustrationId",
+        Illustration: {
+          difficulty: "medium",
+          id: "illustrationId",
+          imageSrc: "imageSrc",
+        },
+        Users: [
+          {
+            id: "bodiId",
+            leaderboardId: "leaderboardId",
+            name: "Bodi",
+            score: 4000,
+          },
+          {
+            id: "joeId",
+            leaderboardId: "leaderboardId",
+            name: "Joe",
+            score: 5000,
+          },
+          {
+            id: "janeId",
+            leaderboardId: "leaderboardId",
+            name: "Jane",
+            score: 6000,
+          },
+        ],
+      },
+    };
+
+    window.fetch = vi.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: vi.fn(() => Promise.resolve(mockLeaderboard)),
+      })
+    );
+
+    const router = createMemoryRouter(
+      [
+        {
+          Component: LeaderboardPage,
+          path: "leaderboards/:leaderboardId",
+        },
+      ],
+      {
+        initialEntries: ["/leaderboards/leaderboardId"],
+      }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const table = await screen.findByRole("table");
+    const tableRows = within(table).getAllByRole("row");
+
+    //  First row that is not column headers row
+    const firstRowCells = within(tableRows[1]).getAllByRole("cell");
+    const secondRowCells = within(tableRows[2]).getAllByRole("cell");
+    const thirdRowCells = within(tableRows[3]).getAllByRole("cell");
+
+    expect(tableRows.length).toBe(4);
+    expect(firstRowCells[0]).toHaveTextContent("1");
+    expect(firstRowCells[1]).toHaveTextContent("Bodi");
+    expect(firstRowCells[2]).toHaveTextContent((4000 / 1000).toFixed(2));
+    expect(secondRowCells[0]).toHaveTextContent("2");
+    expect(secondRowCells[1]).toHaveTextContent("Joe");
+    expect(secondRowCells[2]).toHaveTextContent((5000 / 1000).toFixed(2));
+    expect(thirdRowCells[0]).toHaveTextContent("3");
+    expect(thirdRowCells[1]).toHaveTextContent("Jane");
+    expect(thirdRowCells[2]).toHaveTextContent((6000 / 1000).toFixed(2));
+  });
 });
